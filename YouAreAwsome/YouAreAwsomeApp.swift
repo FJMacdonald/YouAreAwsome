718e894aebc5ab22770d366662171d0df3c3c96d
Francesca Macdonald <104513846+Francesca-Quevita@users.noreply.github.com>
2023-08-14 09:18:01 -0400

    Initial Commit

---
+++ YouAreAwsome/YouAreAwsomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwsomeApp.swift
+//  YouAreAwsome
+//
+//  Created by Francesca MACDONALD on 2023-08-14.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwsomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
